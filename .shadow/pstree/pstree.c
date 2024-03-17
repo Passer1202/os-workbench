b17501d83a6d601f1e47c09b17b125387d5eeebc
tracer-nju <tracer@nju.edu.cn>
2024-03-17 17:46:21 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:46:21 up  1:12,  1 user,  load average: 1.00, 1.00, 0.86

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -11,6 +11,8 @@ typedef struct proc {
   // Add necessary members here
   pid_t pid;
   char* name;
+  char* child;
+  int child_cnt;
   
 } proc;
 
@@ -30,6 +32,28 @@ int pid_max;
 
 const char* base_path="/proc";
 DIR* dir;
+
+void MY_OUT_PUT(proc* p,proc* procs,int d,int pf){
+  if(d>0){
+    int len=(d-1)*4;
+    printf("\n%*s |\n",len,"");
+    printf("%*s",len,"");
+    printf(" +--");
+  }
+  printf("%s",p->name);
+  if(pf){
+    printf("(%d)",p->pid);
+  }
+
+  for(int i=0;i<p->child_cnt;i++){
+    proc* _child=procs;
+    while(_child!=NULL&&(_child->pid!=p->child[i])){
+      _child++;
+    }
+    MY_OUT_PUT(_child,procs,d+1,pf);
+  }
+
+}
 
 
 int main(int argc, char *argv[]) {
@@ -92,7 +116,7 @@ int main(int argc, char *argv[]) {
         strcat(path,ent->d_name);
         strcat(path,"/status");
 
-        printf("%s\n",path);
+        //printf("%s\n",path);
 
         FILE* fp=fopen(path,"r");
 
@@ -117,6 +141,28 @@ int main(int argc, char *argv[]) {
     p++;
   }
 
+  int cnt=p-procs;
+
+  for(int i=0;i<cnt;i++){
+    procs[i].child=malloc(sizeof(pid_t)*cnt);
+    procs[i].child_cnt=0;
+  }
+
+  //建树
+  for(int i=0;i<cnt;i++){
+    for(int j=0;j<cnt;j++){
+      if(procs[i].pid==ppids[j]){
+        int num=procs[i].child_cnt;
+        procs[i].child[num]=procs[j].pid;
+        procs[i].child_cnt++;
+      }
+    }
+  }
+  
+  for(int i=0;i<cnt;i++){
+    if(!ppids[i]) MY_OUT_PUT(&procs[i],procs,0,pf);
+  }
+  
   //printf("nf=%d, pf=%d, vf=%d\n", nf, pf, vf);
 
   return 0;
