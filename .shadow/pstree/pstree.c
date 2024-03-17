d19911654d9436496f46387de54e2cee80e7a8fd
tracer-nju <tracer@nju.edu.cn>
2024-03-17 17:47:44 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:47:44 up  1:14,  1 user,  load average: 1.35, 1.02, 0.87

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -159,9 +159,9 @@ int main(int argc, char *argv[]) {
     }
   }
   
-  for(int i=0;i<cnt;i++){
-    if(!ppids[i]) MY_OUT_PUT(&procs[i],procs,0,pf);
-  }
+  //for(int i=0;i<cnt;i++){
+  //  if(!ppids[i]) MY_OUT_PUT(&procs[i],procs,0,pf);
+  //}
   
   //printf("nf=%d, pf=%d, vf=%d\n", nf, pf, vf);
 
