8358c9c18464d899ecdb70d59673deae222ace00
tracer-nju <tracer@nju.edu.cn>
2024-03-17 12:46:13 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:46:13 up  1:19,  1 user,  load average: 1.00, 0.90, 0.74

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -11,14 +11,40 @@ const struct option opt_table[]={
   {0,0,NULL,0},
 };
 
-
-getopt_long(argc,argv,"-npV",opt_table,NULL);
+int nf;
+int pf;
+int vf;
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+  //for (int i = 0; i < argc; i++) {
+  //  assert(argv[i]);
+  //  printf("argv[%d] = %s\n", i, argv[i]);
+  //}
+  //assert(!argv[argc]);
+  int opt;
+  while(opt=getopt_long(argc,argv,"-npV",opt_table,NULL)!=-1){
+    switch (opt)
+    {
+    case 'n':
+      nf=1;
+      break;
+    case 'p':
+      pf=1;
+      break;
+    case 'V':
+      vf=1;
+      break;
+    case 0:
+      break;
+    case '?':
+      printf("pstree: invalid option -- '%c'\n",optopt);
+      break;
+    default:
+      printf("pstree: invalid option -- '%c'\n",optopt);
+    }
   }
-  assert(!argv[argc]);
+
+  printf("nf=%d, pf=%d, vf=%d\n", nf, pf, vf);
+  
   return 0;
 }
