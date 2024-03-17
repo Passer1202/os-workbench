3d84bb2b7a105319b487e1718479a27d4f79d9c4
tracer-nju <tracer@nju.edu.cn>
2024-03-17 17:09:58 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:58 up 36 min,  1 user,  load average: 0.74, 0.80, 0.68

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,46 +1,120 @@
 
+#include <dirent.h>
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
 #include <getopt.h>
-int nf;
-int pf;
-int vf;
+#include <sys/types.h>
+#include <stdlib.h>
 
+typedef struct proc {
+  // Add necessary members here
+  pid_t pid;
+  char* name;
+  
+} proc;
 
-struct option opt_table[] = {{"show-pids", no_argument, &pf, 1},
-                                  {"numeric-sort", no_argument, &nf, 1},
-                                  {"version", no_argument, &vf, 1},
-                                  {0, 0, 0, 0}};
+extern int pid_max;
+
+const struct option opt_table[]={
+  {"numeric-sort",no_argument,NULL,'n'},
+  {"show-pids",no_argument,NULL,'p'},
+  {"version",no_argument,NULL,'V'},
+  {0,0,NULL,0},
+};
+
+int nf;//nflag
+int pf;//pflag
+int vf;//vflag
+
+const char* base_path="/proc";
+DIR* dir;
 
 
 int main(int argc, char *argv[]) {
-  //for (int i = 0; i < argc; i++) {
-  //  assert(argv[i]);
-  //  printf("argv[%d] = %s\n", i, argv[i]);
-  //}
-  //assert(!argv[argc]);
+
   int opt;
   while((opt=getopt_long(argc,argv,"-npV",opt_table,NULL))!=-1){
     switch (opt)
     {
-    case 'n':
-      nf=1;
-      break;
-    case 'p':
-      pf=1;
-      break;
-    case 'V':
-      vf=1;
-      break;
-    case 0:
-      break;
+    case 'n':nf=1;break;
+    case 'p':pf=1;break;
+    case 'V':vf=1;break;
+    case 0:break;
     default:
       printf("pstree: invalid option -- '%s'\n", argv[optind-1]);
     }
   }
 
-  printf("nf=%d, pf=%d, vf=%d\n", nf, pf, vf);
+  //打开目录
+  
+  if((dir=opendir(base_path))==NULL){
+    printf("pstree: cannot open directory /proc: No such file or directory\n");
+    return -1;
+  }
+
+
+  //保存
+
+  proc* procs=malloc(sizeof(proc)*(pid_max+1));//进程数组
+  pid_t* ppids=malloc(sizeof(pid_t)*pid_max);//父进程的pid
+
+  struct dirent* ent;
+  proc* p=procs;//当前进程指针
+
+  while((ent=readdir(dir))!=NULL){
+      //筛选出为数字的目录
+      if(ent->d_name[0]>='0'&&ent->d_name[0]<='9'){
+        p->pid=atoi(ent->d_name);
+        //p->name=ent->d_name;
+        
+        //todo:
+
+        char* path;
+        int len=strlen(base_path)+strlen(ent->d_name)+7+2;
+        path=malloc(len);
+        memset(path,0,len);
+
+        strcat(path,base_path);
+        strcat(path,"/");
+        strcat(path,ent->d_name);
+        strcat(path,"/status");
+
+        printf("%s\n",path);
+
+        FILE* fp=fopen(path,"r");
+
+        if(fp==NULL){
+          printf("pstree: cannot open file %s: No such file or directory\n",path);
+          return -1;
+        }
+
+        char buf[1024];
+
+        while(fscanf(fp,"%s",buf)!=EOF){
+          if(strcmp(buf,"Name:")==0){
+            fscanf(fp,"%s",p->name);
+          }
+          if(strcmp(buf,"PPid:")==0){
+            fscanf(fp,"%d",&ppids[p-procs]);
+          }
+        }
+        fclose(fp);
+        free(path);
+    }
+    p++;
+  }
+
+  //printf("nf=%d, pf=%d, vf=%d\n", nf, pf, vf);
 
   return 0;
 }
+
+
+
+
+  //for (int i = 0; i < argc; i++) {
+  //  assert(argv[i]);
+  //  printf("argv[%d] = %s\n", i, argv[i]);
+  //}
+  //assert(!argv[argc]);
