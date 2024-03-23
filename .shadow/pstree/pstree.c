35378f97759650f729a010af5bba189e77e8b8e8
tracer-nju <tracer@nju.edu.cn>
2024-03-23 22:20:39 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:20:39 up 18 min,  1 user,  load average: 0.54, 0.25, 0.14

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -40,6 +40,7 @@ void MY_OUT_PUT(proc* p,proc* procs,int d,int pf){
   if(d>0){
     int len=(d-1)*4;
     //printf("\n%*s |\n",len,"");
+    printf("\n");
     printf("%*s",len,"");
     printf(" +--");
   }
