64de91823a6b872e969db96b9442a7a9e1a35b15
tracer-nju <tracer@nju.edu.cn>
2024-04-10 11:09:10 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:09:10 up  1:28,  1 user,  load average: 0.77, 0.62, 0.48

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -1,22 +1,27 @@
 #include "co.h"
 #include <stdlib.h>
+#include <stdint.h>
+#include <setjmp.h>
+
+#define STACK_SIZE 64*1024
 
 enum co_status {
-    CO_NEW = 1, // 新创建，还未执行过
-    CO_RUNNING, // 已经执行过
-    CO_WAITING, // 在 co_wait 上等待
-    CO_DEAD,    // 已经结束，但还未释放资源
+    CO_NEW = 1,                         // 新创建，还未执行过
+    CO_RUNNING,                         // 已经执行过
+    CO_WAITING,                         // 在 co_wait 上等待
+    CO_DEAD,                            // 已经结束，但还未释放资源
 };
 
 struct co {
     char *name;
-    void (*func)(void *); // co_start 指定的入口地址和参数
+    void (*func)(void *);               // co_start 指定的入口地址和参数
     void *arg;
 
-    enum co_status status;  // 协程的状态
-    struct co *    waiter;  // 是否有其他协程在等待当前协程
-    struct context context; // 寄存器现场
-    uint8_t        stack[STACK_SIZE]; // 协程的堆栈
+    enum co_status status;              // 协程的状态
+    struct co *    waiter;              // 是否有其他协程在等待当前协程
+    jmp_buf        context;             // 寄存器现场
+    uint8_t        stack[STACK_SIZE]__attribute__((aligned(16)));   
+                                        // 协程的堆栈,16字节对齐
 };
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
