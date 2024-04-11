59c71458b3bf6bedc9f9b0f75bcb34a0f944b7e4
tracer-nju <tracer@nju.edu.cn>
2024-04-11 13:14:26 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:14:26 up 56 min,  1 user,  load average: 0.69, 0.64, 0.57

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -109,7 +109,7 @@ void co_yield() {
 
             asm volatile(
       #if __x86_64__
-                "movq %%rdi, (%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
+                " movq %0, %%rsp; movq %2, %%rdi; call *%1"
                 :
                 : "b"((uintptr_t)(choice->stack + sizeof(choice->stack))), "d"(choice->func), "a"((uintptr_t)(choice->arg))
                 : "memory"
@@ -121,19 +121,7 @@ void co_yield() {
       #endif
       );
 
-      asm volatile(
-      #if __x86_64__
-                "movq (%0), %%rdi"
-                :
-                : "b"((uintptr_t)(choice->stack + sizeof(choice->stack)))
-                : "memory"
-      #else
-                "movl 0x8(%0), %%esp; movl 0x4(%0), %%ecx"
-                :
-                : "b"((uintptr_t)(choice->stack + sizeof(choice->stack) - 8))
-                : "memory"
-      #endif
-      );
+      
 
       choice->status = CO_DEAD;
 
@@ -145,9 +133,9 @@ void co_yield() {
     } else if (choice->status == CO_RUNNING) {
       longjmp(choice->context, 1);
     } 
-    //else {
-    //  assert(0);
-    //}
+    else {
+      assert(0);
+    }
 
 }
 
