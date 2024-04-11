9ed0b9205d0a216fae15b4bc28d66a3d39841ce8
tracer-nju <tracer@nju.edu.cn>
2024-04-11 13:12:30 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:12:30 up 54 min,  1 user,  load average: 0.72, 0.60, 0.55

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -107,20 +107,19 @@ void co_yield() {
     if (choice->status == CO_NEW) {
       choice->status = CO_RUNNING;
 
-      asm volatile(
-        #if __x86_64__
-                    "movq %%rdi, (%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
-                    :
-                    : "b"((uintptr_t)(choice->stack + sizeof(choice->stack))), "d"(choice->func), "a"((uintptr_t)(choice->arg))
-                    : "memory"
-        #else
-                    "movl %%esp, 0x8(%0); movl %%ecx, 0x4(%0); movl %0, %%esp; movl %2, (%0); call *%1"
-                    :
-                    : "b"((uintptr_t)(choice->stack + sizeof(choice->stack) - 8)), "d"(choice->func), "a"((uintptr_t)(choice->arg))
-                    : "memory" 
-        #endif
-        );
-
+            asm volatile(
+      #if __x86_64__
+                "movq %%rdi, (%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
+                :
+                : "b"((uintptr_t)(choice->stack + sizeof(choice->stack))), "d"(choice->func), "a"((uintptr_t)(choice->arg))
+                : "memory"
+      #else
+                "movl %%esp, 0x8(%0); movl %%ecx, 0x4(%0); movl %0, %%esp; movl %2, (%0); call *%1"
+                :
+                : "b"((uintptr_t)(choice->stack + sizeof(choice->stack) - 8)), "d"(choice->func), "a"((uintptr_t)(choice->arg))
+                : "memory" 
+      #endif
+      );
 
       asm volatile(
       #if __x86_64__
