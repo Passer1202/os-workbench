4f79a2b4db712cb2e80c7a04edfccf747a2bc664
tracer-nju <tracer@nju.edu.cn>
2024-04-11 15:21:29 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:21:29 up  1:25,  1 user,  load average: 0.51, 0.46, 0.42

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -110,9 +110,9 @@ void co_yield() {
 
       asm volatile(
       #if __x86_64__
-                "movq %%rdi, 0x16(%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
+                "movq %%rdi, 0x32(%0); movq %0, %%rsp; movq %2, %%rdi; call *%1"
                 :
-                : "b"((uintptr_t)(choice->stack + sizeof(choice->stack)-16)), "d"(choice->func), "a"((uintptr_t)(choice->arg))
+                : "b"((uintptr_t)(choice->stack + sizeof(choice->stack)-32)), "d"(choice->func), "a"((uintptr_t)(choice->arg))
                 : "memory"
       #else
                 "movl %%esp, 0x8(%0); movl %%ecx, 0x4(%0); movl %0, %%esp; movl %2, (%0); call *%1"
