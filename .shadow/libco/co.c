8efe0ec94a6593cd05ec5e1c6dd5c64744221b84
tracer-nju <tracer@nju.edu.cn>
2024-04-12 15:15:23 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:15:22 up  1:05,  1 user,  load average: 1.27, 0.68, 0.53

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -13,6 +13,28 @@
 
 #define NAME_SIZE 64
 
+void wrapper(void *sp, void *entry, uintptr_t arg) {
+    // stack switching, in assembly
+
+     asm volatile(
+      #if __x86_64__
+                "movq %0, %%rsp; "
+                :
+                : "b"((uintptr_t)(sp))
+                : "memory"
+      #else
+                "movl %0, %%esp; movl %2, 4(%0)"
+                :
+                : "b"((uintptr_t)(sp - 8)), "d"(choice->func), "a"((uintptr_t)(choice->arg))
+                : "memory" 
+      #endif
+      );
+
+
+    ((void(*)(uintptr_t))entry)(arg);
+    // coroutine goes here after entry(arg)
+}
+
 
 enum co_status {
     CO_NEW = 1,                         // 新创建，还未执行过
@@ -108,19 +130,7 @@ void co_yield() {
     if (choice->status == CO_NEW) {
       choice->status = CO_RUNNING;
 
-      asm volatile(
-      #if __x86_64__
-                "movq %0, %%rsp; movq %2, %%rdi; call *%1"
-                :
-                : "b"((uintptr_t)(choice->stack + sizeof(choice->stack))), "d"(choice->func), "a"((uintptr_t)(choice->arg))
-                : "memory"
-      #else
-                "movl %0, %%esp; movl %2, 4(%0); call *%1"
-                :
-                : "b"((uintptr_t)(choice->stack + sizeof(choice->stack) )), "d"(choice->func), "a"((uintptr_t)(choice->arg))
-                : "memory" 
-      #endif
-      );
+      wrapper((uintptr_t)(choice->stack + sizeof(choice->stack)),choice->func,choice->arg);
 
 
       choice->status = CO_DEAD;
