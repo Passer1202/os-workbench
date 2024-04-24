9766b4eec7a68130fb59806fb2cf10c57cf9f67c
tracer-nju <tracer@nju.edu.cn>
2024-04-24 11:08:21 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:08:21 up  1:14,  1 user,  load average: 0.86, 0.85, 0.87

--- .shadow/abstract-machine/klib/src/stdlib.c
+++ .shadow/abstract-machine/klib/src/stdlib.c
@@ -29,12 +29,36 @@ int atoi(const char* nptr) {
   return x;
 }
 
+void* stack_ptr() {
+  void* sp;
+  asm volatile("mov %%rsp, %0" : "=r"(sp));
+  return sp;
+}
+
 void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
+  // On native, malloc() will be called during initialization of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  extern char end;
+  static char *heap_end = &end;
+  char *prev_heap_end;
+  char *brk;
+
+  if (size == 0) {
+    return NULL;
+  }
+
+  prev_heap_end = heap_end;
+  brk = prev_heap_end + size;
+
+  if (brk > (char *)stack_ptr()) {
+    panic("out of memory");
+  }
+
+  heap_end = brk;
+  return prev_heap_end;
+
 #endif
   return NULL;
 }
