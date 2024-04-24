5e48f5ff1f5c0004987df012771501e713f05af4
tracer-nju <tracer@nju.edu.cn>
2024-04-24 10:25:22 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:25:22 up 31 min,  1 user,  load average: 0.72, 0.94, 0.80

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,9 +1,43 @@
 #include <common.h>
 
+//TODO：自定测试框架
+
+//锁的状态
+enum LOCK_STATE {
+    UNLOCKED=0, LOCKED
+};
+
+//int atomic_xchg(volatile int *addr, int newval);
+//原子 (不会被其他处理器的原子操作打断) 地交换内存地址中的数值,返回原来的值
+
+//初始化锁
+static void init_lock(int * lock){
+    atomic_xchg(lock, UNLOCKED);
+}
+
+//自旋直到获取锁
+static void get_lock(int * lock){
+    while(atomic_xchg(lock, LOCKED) == LOCKED);
+}
+
+//释放锁
+static void release_lock(int * lock){
+    assert(atomic_xchg(lock, UNLOCKED)==LOCKED);
+}
+
+//尝试获取锁
+static int try_lock(int * lock){
+    return atomic_xchg(lock, LOCKED);
+}
+
 static void *kalloc(size_t size) {
     // TODO
     // You can add more .c files to the repo.
-
+    int lock;
+    init_lock(&lock);
+    get_lock(&lock);
+    release_lock(&lock);
+    try_lock(&lock);
     return NULL;
 }
 
