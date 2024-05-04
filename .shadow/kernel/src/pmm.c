0a0f0eba0aa822ae7975eb3416c72f9af1c24bfa
tracer-nju <tracer@nju.edu.cn>
2024-05-04 11:18:33 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:18:33 up 48 min,  1 user,  load average: 2.15, 1.50, 0.98

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -457,11 +457,7 @@ void test_pmm() {
     
     while(1){
     alloc(32);
-    void* a=kalloc(4096);
-    atomic{
-    printf("CPU #%d :kalloc 4096\n",cpu_current());
-    }
-    kfree((void*)a);
+    
     }
     
 }
