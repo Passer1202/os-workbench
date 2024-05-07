530ebcddb26debb706ce8ecaf2e0557154b4f18e
tracer-nju <tracer@nju.edu.cn>
2024-05-07 16:39:27 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:39:27 up 6 min,  1 user,  load average: 1.00, 0.88, 0.44

--- .shadow/kernel/include/common.h
+++ .shadow/kernel/include/common.h
@@ -3,45 +3,4 @@
 #include <klib-macros.h>
 
 void test_pmm();
-
-//参考了WYY的定义
-//未分配内存和已分配内存可以共用这个 header，
-//当内存块处于未分配状态时，内存块被放在链表中，
-//union 字段存放的是下一个链表节点的地址；
-//当内存块处于已分配状态时，union 字段存放魔数。
-//一个 kfree() 地址来临时，
-//只要查看其前一个 uintptr_t 中是不是 ALLOC_MAGIC，
-//就能确定这个地址合不合法，再往前看就能获得内存块的 size。
-
-typedef struct __header_t {
-    size_t sz;
-    union {
-        uintptr_t magic;
-        struct __header_t *next;
-    };
-}header_t;
-
-//魔数
-#define MAGIC_NUM 0X1234567
-//cpu数量不超过8个
-#define CPU_MAX 8
-//分配的最小内存块大小
-#define MIN_SIZE  32
-//16KB
-#define _16KB  16*1024
-//64KB
-#define _64KB  64*1024
-//16MB
-#define _16MB  16*1024*1024
-
-/*定义锁lock_t*/
-typedef int lock_t;
-
-/*锁的状态*/
-enum LOCK_STATE {
-    UNLOCKED=0, LOCKED
-};
-
-/*锁的API*/
-//int atomic_xchg(volatile int *addr, int newval);
-//原子 (不会被其他处理器的原子操作打断) 地交换内存地址中的数值,返回原来的值
+//一把大锁保平安（速通版）
--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,50 +1,98 @@
 #include <common.h>
+
 //TODO：自定测试框架
 
-//重构代码！！！！
+//锁的状态
+enum LOCK_STATE {
+    PMM_UNLOCKED=0, PMM_LOCKED
+};
+
+#define MAGIC_NUM 0X1234567
+
+//int atomic_xchg(volatile int *addr, int newval);
+//原子 (不会被其他处理器的原子操作打断) 地交换内存地址中的数值,返回原来的值
 
 
+typedef int pmm_lock_t;
+
+
+//#define TEST
+//4GiB
+#define HEAP_SIZE (4LL << 30)
+
+//初始化锁
+static void init_lock(int * lock){
+    atomic_xchg(lock, PMM_UNLOCKED);
+}
+//自旋直到获取锁
+static void get_lock(int * lock){
+    while(atomic_xchg(lock, PMM_LOCKED) == PMM_LOCKED);
+}
+//释放锁
+static void release_lock(int * lock){
+    assert(atomic_xchg(lock, PMM_UNLOCKED)==PMM_LOCKED);
+}
+//尝试获取锁
+static int try_lock(int * lock){
+    return atomic_xchg(lock, PMM_LOCKED);
+}
 
 static void *kalloc(size_t size) {
-   return NULL;
+
+    static char* pos;
+
+    char* ret;
+
+    int sz=1;
+
+    if(!pos)pos=heap.start;
+
+    while(sz<size){
+        sz*=2;
+    }
+
+    while((intptr_t)pos%sz!=0){
+        pos++;
+    }
+    
+    ret=pos;
+
+    pos+=sz;
+
+    assert(pos <= (char*)heap.end);
+
+    return ret;
 }
-//有死锁
+
 static void kfree(void *ptr) {
-    return;
+    // TODO
+    // You can add more .c files to the repo.
 }
 
 
-
+#ifndef TEST
 // 框架代码中的 pmm_init (在 AbstractMachine 中运行)
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
-  
+
 }
-
-
-void alloc(int sz){
-    
-    uintptr_t a=(uintptr_t)kalloc(sz);
-
-    uintptr_t align=a & -a ;
-
-    //atomic{
-    printf("CPU #%d : Alloc %d -> %p align = %d\n", cpu_current(),sz, a, align);
-    //}
-
-    assert(a&&align>=sz);
+#else
+// 测试代码的 pmm_init ()
+static void pmm_init() {
+  char *ptr  = malloc(HEAP_SIZE);
+  heap.start = ptr;
+  heap.end   = ptr + HEAP_SIZE;
+  printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
 }
+#endif
 
 void test_pmm() {
-   
-    alloc(1);
-    alloc(5);
-    alloc(10);
-    alloc(32);
-    while(1){
-        alloc(4096);
-    }
+    pmm_lock_t pmm_lock;
+    init_lock(&pmm_lock);
+    get_lock(&pmm_lock);
+    release_lock(&pmm_lock);
+    try_lock(&pmm_lock);
 }
 
 MODULE_DEF(pmm) = {
