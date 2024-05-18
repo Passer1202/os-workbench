ff31847ad34bfd66d7f86feb1cb13eead70c98a5
tracer-nju <tracer@nju.edu.cn>
2024-05-18 15:47:33 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:47:33 up  1:50,  1 user,  load average: 1.05, 0.88, 0.83

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -10,6 +10,33 @@
 
 const char *lib_name = "/tmp/mylib.so";
 
+void run_cmd(const char *cmd){
+
+     // 创建子进程
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return ;
+    } else if (pid == 0) {  // 子进程
+        // 在子进程中执行编译命令
+        execl("/bin/sh", "/bin/sh", "-c", cmd, NULL);
+        // 如果exec失败则打印错误并退出
+        perror("execl");
+        exit(1);
+    } else {  // 父进程
+        // 等待子进程结束
+        int status;
+        waitpid(pid, &status, 0);
+        // 检查子进程退出状态
+        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+            fprintf(stderr, "Compilation failed\n");
+            return ;
+        }
+    }
+
+
+}
+
 void init(){
 
     //初始化，生成一个空的共享库
@@ -30,37 +57,19 @@ void init(){
 
     // 2. 编译源代码文件
 
-    char compile_command[256];
-    snprintf(compile_command, sizeof(compile_command), "gcc -shared -o %s %s",lib_name, source_filename);
+    char cmd[256];
 
+    snprintf(cmd, sizeof(cmd), "gcc -shared -o %s %s",lib_name, source_filename);
     //gcc -shared -o my_library.so my_library.c
+    run_cmd(cmd);
 
-    // 创建子进程
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        return ;
-    } else if (pid == 0) {  // 子进程
-        // 在子进程中执行编译命令
-        execl("/bin/sh", "/bin/sh", "-c", compile_command, NULL);
-        // 如果exec失败则打印错误并退出
-        perror("execl");
-        exit(1);
-    } else {  // 父进程
-        // 等待子进程结束
-        int status;
-        waitpid(pid, &status, 0);
-        // 检查子进程退出状态
-        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-            fprintf(stderr, "Compilation failed\n");
-            return ;
-        }
-    }
+    // 3. 清理临时文件
+    remove(source_filename);
 
 }
 
 bool is_function(const char *s) {
-    // To be implemented.
+    //目前的实现是必须‘int ’开头
     char *result = strstr(s, "int");
     if(result != NULL){
         if(s[0] == 'i' && s[1] == 'n' && s[2] == 't' && s[3] == ' ')
@@ -88,10 +97,69 @@ int main(int argc, char *argv[]) {
         }
         
         if(is_function(line)){
-            printf("is function\n");
+            //输入是函数;
+
+            // 1. 创建临时源代码文件
+            const char *source_code = line;
+            const char *source_filename = "/tmp/temp_code.c";
+            FILE *source_file = fopen(source_filename, "w");
+            if (source_file == NULL) {
+                perror("fopen");
+                return 1;
+            }
+            fprintf(source_file, "%s", source_code);
+            fclose(source_file);
+
+            // 2. 编译源代码文件
+            const char *library_filename = "/tmp/temp_code.o";
+            char cmd[256];
+            //gcc -c -fPIC new_function.c -o new_function.o
+            snprintf(cmd, sizeof(cmd), "gcc -c -fPIC %s -o %s", source_filename,library_filename);
+
+            run_cmd(cmd);
+
+            //gcc -shared -o liboriginal.so -Wl,--whole-archive liboriginal.so -Wl,--no-whole-archive new_function.o
+
+
+            snprintf(cmd, sizeof(cmd), "gcc -shared -o %s -Wl,--whole-archive %s -Wl,--no-whole-archive %s", lib_name, lib_name, library_filename);
+
+            run_cmd(cmd);
+
+            // 3. 清理临时文件
+            remove(source_filename);
+            remove(library_filename);
+
         }
         else{
-            printf("not function\n");
+            //输入的应该是表达式
+            void *handle;
+            int (*foo)(void);  // 假设foo是一个无参数且返回void的函数
+            char *error;
+
+            // 打开共享库
+            handle = dlopen(lib_name, RTLD_LAZY);
+            if (!handle) {
+                fprintf(stderr, "%s\n", dlerror());
+                return 1;
+            }
+
+            // 清除现有的错误
+            dlerror();
+
+            // 获取foo函数的地址
+            *(void **) (&foo) = dlsym(handle, line);
+            if ((error = dlerror()) != NULL)  {
+                fprintf(stderr, "%s\n", error);
+                dlclose(handle);
+                return 1;
+            }
+
+            // 调用函数
+            printf("%d\n", foo());
+
+            // 关闭共享库
+            dlclose(handle); 
+
         }
         
         
