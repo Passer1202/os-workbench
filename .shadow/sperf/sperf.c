c0857609e1494da6f418d7e211e071c5517bcd73
tracer-nju <tracer@nju.edu.cn>
2024-05-22 14:19:35 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:35 up 20 min,  1 user,  load average: 1.02, 0.62, 0.47

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    char *exec_argv[] = { "strace", "ls", NULL, };
+    char *exec_envp[] = { "PATH=/bin", NULL, };
+    execve("strace",          exec_argv, exec_envp);
+    execve("/bin/strace",     exec_argv, exec_envp);
+    execve("/usr/bin/strace", exec_argv, exec_envp);
+    perror(argv[0]);
+    exit(EXIT_FAILURE);
+}
+/*
+int main(int argc, char *argv[]) {
+
+    
     for (int i = 0; i < argc; i++) {
         assert(argv[i]);
         printf("argv[%d] = %s\n", i, argv[i]);
@@ -9,3 +23,4 @@ int main(int argc, char *argv[]) {
     assert(!argv[argc]);
     return 0;
 }
+*/
