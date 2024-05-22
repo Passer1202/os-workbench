2599ed15f9414e054a8f478e2d818291090e8dfc
tracer-nju <tracer@nju.edu.cn>
2024-05-22 15:57:42 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:57:42 up  1:58,  1 user,  load average: 0.63, 0.92, 0.85

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <regex.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,6 +7,14 @@
 #include <sys/wait.h>
 
 //#define DEBUG 0
+
+void extract_syscall_times(const char *strace_output) {
+    
+
+
+    
+}
+
 
 #ifndef DEBUG
 int main(int argc, char *argv[]) {
@@ -53,12 +62,34 @@ int main(int argc, char *argv[]) {
 
         while(1){
             fflush(stdout);
+            
             if (!fgets(buf, sizeof(buf), fp)) {
                 break;
             }
             //目前看父进程会结束，先这样写
             
-            printf("%s", buf);
+            //正则表达式
+            regex_t regex_name,regex_time;
+            regmatch_t match_name,match_time;
+
+            const char pattern_name[] = "[^\\(\n\r\b\t]*\\(";
+            const char pattern_time[] = "<[0-9\\.]+>";
+
+            assert(regcomp(&regex_name, pattern_name, REG_EXTENDED)==0);
+            assert(regcomp(&regex_time, pattern_time, REG_EXTENDED)==0);
+
+            //执行正则匹配
+            int ret_name = regexec(&regex_name, buf, 1, &match_name, 0);
+            int ret_time = regexec(&regex_time, buf, 1, &match_time, 0);
+            if (ret_name == 0 && ret_time == 0) {
+                //提取匹配的系统调用名和时间
+                char syscall[64];
+                char time[64];
+                snprintf(syscall, match_name.rm_eo - match_name.rm_so + 1, "%s", buf + match_name.rm_so);
+                snprintf(time, match_time.rm_eo - match_time.rm_so + 1, "%s", buf + match_time.rm_so);
+                printf("Syscall: %s, Time: %s\n", syscall, time);
+            }
+
             //输出是一行行来的
         }
     }
