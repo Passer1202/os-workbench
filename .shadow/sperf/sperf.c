48e6b062e87b8ff562d9a20870d6f2d4371d0bc8
tracer-nju <tracer@nju.edu.cn>
2024-05-22 14:40:03 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:03 up 41 min,  1 user,  load average: 1.46, 1.07, 0.82

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -2,17 +2,43 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
+#define DEBUG 0
+
+#ifndef DEBUG
 int main(int argc, char *argv[]) {
-    char *exec_argv[] = { "strace", "ls", NULL, };
-    char *exec_envp[] = { "PATH=/bin", NULL, };
-    execve("a",          exec_argv, exec_envp);
-    execve("/bin/strace",     exec_argv, exec_envp);
-    //execve("/usr/bin/strace", exec_argv, exec_envp);
-    perror(argv[0]);
-    exit(EXIT_FAILURE);
+    /*创建匿名管道，子进程写，父进程读*/
+    int pipefd[2];
+    
+    //创建失败返回-1
+    assert(pipe(pipefd) != -1);
+    //创建子进程
+    pid_t pid = fork();
+    //创建失败返回-1
+    assert(pid != -1);
+
+    if (pid == 0) {
+        //子进程   //执行strace命令
+        assert(close(pipefd[0])!=-1);
+        assert(dup2(pipefd[1], STDOUT_FILENO)!=-1);
+        assert(close(pipefd[1])!=-1);
+        execvp(argv[1], argv + 1);
+        perror("execvp");
+        exit(1);
+    } else {
+        //父进程    //读strace的输出并处理
+        close(pipefd[1]);
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[0]);
+        execvp(argv[2], argv + 2);
+        perror("execvp");
+        exit(1);
+    }
+    return 0;
 }
-/*
+#else
 int main(int argc, char *argv[]) {
 
     
@@ -23,4 +49,4 @@ int main(int argc, char *argv[]) {
     assert(!argv[argc]);
     return 0;
 }
-*/
+#endif
