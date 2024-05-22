325b43fcb94c6c74ca7545929f8b866ee5a3c91e
tracer-nju <tracer@nju.edu.cn>
2024-05-22 19:38:44 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:38:44 up  5:39,  1 user,  load average: 1.07, 1.02, 0.98

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -111,8 +111,6 @@ int main(int argc, char *argv[]) {
         double total_time = 0;
         int run_flag=1;
 
-         assert(0);
-         
         while(run_flag==1){
             while (fgets(buf, 4096, fp) > 0) 
             {
@@ -135,7 +133,7 @@ int main(int argc, char *argv[]) {
 
                 assert(regcomp(&regex_name, pattern_name, REG_EXTENDED)==0);
                 assert(regcomp(&regex_time, pattern_time, REG_EXTENDED)==0);
-
+                 assert(0);
                 //执行正则匹配
                 int ret_name = regexec(&regex_name, buf, 1, &match_name, 0);
                 int ret_time = regexec(&regex_time, buf, 1, &match_time, 0);
