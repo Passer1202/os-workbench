c1e27d49eefc3fbd73590eeaa56b9df91064b397
tracer-nju <tracer@nju.edu.cn>
2024-07-06 17:55:14 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:55:14 up  8:50,  1 user,  load average: 0.86, 0.81, 0.88

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -159,12 +159,13 @@ int main(int argc, char *argv[]) {
                         while(img_sz >= CLUS_SIZE(hdr)){
                             printf("name: %s\n", name);
                             printf("img_sz: %d\n", img_sz);
+                            printf("img_current: %p\n", img_current);
                             printf("CLUS_SIZE(hdr): %d\n", CLUS_SIZE(hdr));
 
                             fwrite((void *)img_current, CLUS_SIZE(hdr), 1, bmp_tmp_file);
 
-                            img_current =img_current+ CLUS_SIZE(hdr);
-                            img_sz = img_sz- CLUS_SIZE(hdr);
+                            img_current =img_current+ 4096;
+                            img_sz = img_sz- 4096;
 
                         }
                         if(img_sz > 0){
