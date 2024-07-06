e8216ef1175de8a97558662aba0816479e9d39bd
tracer-nju <tracer@nju.edu.cn>
2024-07-06 18:25:24 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:25:24 up  9:20,  1 user,  load average: 1.25, 0.97, 0.77

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -148,7 +148,7 @@ int main(int argc, char *argv[]) {
                     struct bmp_info_header *bmp_ihdr = (struct bmp_info_header *)(bmp_hdr + 1);
                     fwrite(bmp_ihdr, sizeof(struct bmp_info_header), 1, bmp_tmp_file);
                     //写入bmp图像数据
-                    uintptr_t img_start = ((uintptr_t)bmp_hdr + IMG_OFFSET(bmp_hdr));
+                    uintptr_t img_start = (uintptr_t)bmp_hdr + IMG_OFFSET(bmp_hdr);
 
                     assert(bmp_hdr->bfType == 0x4d42);//确定是bmp文件
                     assert(bmp_ihdr->biSize == 40);//信息头大小为40
@@ -157,7 +157,8 @@ int main(int argc, char *argv[]) {
                        fwrite((void *)img_start, IMG_SIZE(bmp_ihdr), 1, bmp_tmp_file);
                     }else{
                         //该文件占了多个簇
-                        
+
+                        printf("rest size: %d\n", REST_SIZE(hdr));
                         fwrite((void *)img_start, REST_SIZE(hdr), 1, bmp_tmp_file);
 
                         int img_sz = IMG_SIZE(bmp_ihdr) - REST_SIZE(hdr);
