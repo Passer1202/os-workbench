f3394a9dd7f07b3829edcddc4752475666d15b7f
tracer-nju <tracer@nju.edu.cn>
2024-07-08 15:48:39 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:48:39 up 20 min,  1 user,  load average: 0.76, 0.72, 0.54

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -3,21 +3,17 @@
 //TODO：提速
 //
 //bug:虚拟机会神秘重启
-//魔数
-#define MAGIC_NUM 0X1234567
-//cpu数量不超过8个
-#define CPU_MAX 8
-//分配的最小内存块大小
-#define MIN_SIZE  16
-//4KB
-#define _4KB  4*1024
-//64KB
-#define _64KB  64*1024
-//16MB
-#define _16MB  16*1024*1024
+//目前来看是kfree存在问题
+
+
+
 
 /*定义锁lock_t*/
 typedef int lock_t;
+
+void *cpu_ptr[8]; 
+void *cpu_ptr_end[8];
+
 
 /*锁的状态*/
 enum LOCK_STATE {
@@ -26,6 +22,8 @@ enum LOCK_STATE {
 
 /*锁们*/
 //堆区的大锁，slowpath
+lock_t cpu_lock[8];
+
 lock_t heap_lock;
 
 /*锁的API*/
@@ -54,280 +52,49 @@ static void release_lock(int * lock){
     for (int _cnt = (acquire_lock(&heap_lock),0); _cnt < 1; _cnt++ , release_lock(&heap_lock))
 
 
-//hader_t参考了WYY的定义
-typedef struct __header_t {
-    size_t sz;
-    union {
-        uintptr_t magic;
-        struct __header_t *next;
-    };
-}header_t;
-
-//堆区的头节点
-header_t *head=NULL;
-
-//每个page的头节点，指示当前page的slab大小以及下一页page的地址
-typedef struct __page_header_t {
-    size_t sz;
-    struct __page_header_t *next;
-    void* first_slab;
-}pheader_t;
-
-lock_t cpu_page_lock[CPU_MAX];
-
-typedef pheader_t cpheader_t;
-cpheader_t* cpu_page[CPU_MAX];
-
 
 static void *kalloc(size_t size) {
-    
-    if(size>_16MB){
-        return NULL;
-    }
-
-    //assert(size<=_16MB);
-
-    size_t sz=MIN_SIZE;
-    while(sz<size){
-            sz<<=1;
-    }
-
-    //assert(sz>=MIN_SIZE);
-    //assert(sz>=size);
-
-    if(sz>_4KB){
-        //(4KB,16MB]
-        //大内存分配,一把大锁保平安
-        acquire_lock(&heap_lock);
-        //printf("CPU #%d : Alloc %d\n", cpu_current(),sz);
-
-        header_t *p=head;
-        header_t *pre=NULL;
-
-        while(p){
-            if(p->sz>=sz+sizeof(header_t)){
-                char* pos=(char*)p+sizeof(header_t)+p->sz-sz;
-                while((intptr_t)pos%sz!=0){
-                    pos--;
-                }
-                if(pos>=(char*)p+sizeof(header_t)){
-                    //此时pos有两种情况
-                    //assert(pos>=((char*)p+sizeof(header_t)));
-
-                    if(pos<((char*)p+2*sizeof(header_t))){
-                        //原来的header_t要作废，形成空洞
-                        if(pre){
-                            assert(pre->next==p);
-                            pre->next=p->next;
-                            //跳了过去
-                        }
-                        else{
-                            assert(p==head);
-                            head=p->next;
-                        }
-
-                    }
-                    else{
-                        //原来的header_t不会被破坏，只需要略加维护
-                        //此时原header_t对应的size可能是0
-                        p->sz=pos-2*sizeof(header_t)-(char*)p;
-                    }
-                    header_t *new=(header_t*)pos-sizeof(header_t);
-                    new->sz=sz;
-                    new->magic=MAGIC_NUM;
-                    //p<pos-sizeof(header_t)<p+sizeof(header_t)
-                    //p-sizeof(header_t)>p+sizeof(header_t)
-
-                    //返回pos
-                    release_lock(&heap_lock);
-                    return (void*)pos;
-                }
-            }
-            pre=p;
-            p=p->next;
-        }
-
-        release_lock(&heap_lock);
-        return NULL;
-    }
-    //[16B,4KB]
-
-    //没slab或slab满了就分配一个slab
     int cpu_now=cpu_current();
-    acquire_lock(&cpu_page_lock[cpu_now]);
+
+    size_t sz=1;
     
-    if(cpu_page[cpu_now]==NULL){
-        //分配一页64KB的内存
-        //void* newslab=alloc_page();
-        release_lock(&cpu_page_lock[cpu_now]);
-        void *newslab=kalloc(_64KB);
-        acquire_lock(&cpu_page_lock[cpu_now]);
+    acquire_lock(&cpu_lock[cpu_now]);
 
-        if(newslab==NULL){
-            release_lock(&cpu_page_lock[cpu_now]);
-            return NULL;
-        }
-
-        //初始化slab_page
-
-        //先不考虑释放
-        //不停往前走
-        pheader_t* ph=(pheader_t*)newslab;
-        cpu_page[cpu_now]=ph;
-
-        ph->sz=sz;
-        ph->next=NULL;
-        ph->first_slab=(void*)ph+sizeof(pheader_t);
-        while((intptr_t)ph->first_slab%sz!=0){
-            ph->first_slab++;
-        }
-
-        //找到第一个slab
-
-        void *ret=ph->first_slab;
-        ph->first_slab+=sz;
-        release_lock(&cpu_page_lock[cpu_now]);
-        return ret;
-
-        //分配第一个slab
-
+    while(sz<size){
+        sz*=2;
     }
-    else{
-        //先找有没有slab页满足要求
-        pheader_t* ph=cpu_page[cpu_now];
-        while(ph){
-            if(ph->sz==sz){
-                if(ph->first_slab+sz<(void*)ph + _64KB){
-                    break;
-                }
-            }
-            ph=ph->next;
-        }
-        if(ph){
-            //找到了
-            void *ret=ph->first_slab+sz;
-            ph->first_slab+=sz;
-            release_lock(&cpu_page_lock[cpu_now]);
-            return ret;
-        }
-        else{
-            void *newslab=kalloc(_64KB);
 
-            if(newslab==NULL){
-                release_lock(&cpu_page_lock[cpu_now]);
-                return NULL;
-            }
-            pheader_t* nph=(pheader_t*)newslab;
-            nph->next=cpu_page[cpu_now];
-            cpu_page[cpu_now]=nph;
-            nph->sz=sz;
-            nph->first_slab=(void*)nph+sizeof(pheader_t);
-            while((intptr_t)nph->first_slab%sz!=0){
-                nph->first_slab++;
-            }
-            void *ret=nph->first_slab;
-            nph->first_slab+=sz;
-            release_lock(&cpu_page_lock[cpu_now]);
-            return ret;
-        }
-
+    if(sz>(1<<24)){
+        release_lock(&cpu_lock[cpu_now]);
+        return NULL;
     }
+
+    if(cpu_ptr[cpu_now]+sz>cpu_ptr_end[cpu_now]){
+        release_lock(&cpu_lock[cpu_now]);
+        return NULL;
+    }
+
+    static char* p;
+    
+    if(!p){
+        p=cpu_ptr[cpu_now];
+    }
+
+    
+    while((intptr_t)p%sz!=0){
+        p++;
+    }
+    
+    char* ret=p;
+    p+=sz;
+    release_lock(&cpu_lock[cpu_now]);
+    return ret;
+    
 
 }
 
 static void kfree(void *ptr) {
-    //slab我先不还
-    int cpu_no=0;
-    for(;cpu_no<cpu_count();cpu_no++){
-        acquire_lock(&cpu_page_lock[cpu_no]);
-        pheader_t *ph=cpu_page[cpu_no];
-        while(ph){
-            if(ptr>=(void*)ph && ptr<(void*)ph+_64KB){
-                //找到了
-                //assert((intptr_t)ptr%(ph->sz)==0);
-                //assert((intptr_t)ptr%(ph->sz)==0);
-                //assert((intptr_t)ptr%(ph->sz)==0);
-                //assert((intptr_t)ptr%(ph->sz)==0);
-                release_lock(&cpu_page_lock[cpu_no]);
-                //assert(0);
-                return;
-            }
-            ph=ph->next;
-        }
-        release_lock(&cpu_page_lock[cpu_no]);
-    }
-    //大内存
-    acquire_lock(&heap_lock);
-    header_t *p=head;
-    header_t *pre=NULL;
-    header_t *aft=NULL;
-    while(p){
-        if(ptr>(void*)p){
-            if(pre){
-                pre=pre>p?pre:p;
-            }
-            else
-                pre=p;
-        }
-        else if(ptr<(void*)p){
-            if(aft){
-                aft=aft<p?aft:p;
-            }
-            else
-                aft=p;
-        }
-        p=p->next;
-    }
-    header_t *cur=(header_t*)((char*)ptr-sizeof(header_t));
-
-    if(pre&&(void*)pre+pre->sz+sizeof(header_t)==(void*)cur){
-        //合并
-        pre->sz=pre->sz+sizeof(header_t)+cur->sz;
-        if(aft&&(void*)cur+cur->sz+sizeof(header_t)==(void*)aft){
-            pre->sz=pre->sz+sizeof(header_t)+aft->sz;
-            p=head;
-            while(p->next){
-                if(p->next==aft){
-                    break;
-                }
-                p=p->next;
-            }
-            if(p&&p->next==aft){
-                //aft是头
-                head=aft->next;
-            }
-            else{
-                p->next=aft->next;
-            }
-        }
-    }
-    else if(aft&&(void*)cur+cur->sz+sizeof(header_t)==(void*)aft){
-        //合并
-        cur->sz=cur->sz+sizeof(header_t)+aft->sz;
-        p=head;
-        while(p->next){
-            if(p->next==aft){
-                break;
-            }
-            p=p->next;
-        }
-        if(p&&p->next==aft){
-            //aft是头
-            head=aft->next;
-        }
-        else{
-            p->next=aft->next;
-        }
-        cur->next=head;
-        head=cur;
-    }
-    else{
-        cur->next=head;
-        head=cur;
-    }
-    release_lock(&heap_lock);
-    //assert(0);
-    return;
+   
 }
 
 
@@ -335,22 +102,23 @@ static void kfree(void *ptr) {
 // 框架代码中的 pmm_init (在 AbstractMachine 中运行)
 static void pmm_init() {
     uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
-    printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+    int cpu_cnt=cpu_count();
+    intptr_t cpu_sz=pmsize/cpu_cnt;
 
-    //维护空闲节点
-    head = (header_t *)heap.start;
-    head->sz = pmsize-sizeof(header_t);//减去头部大小
-    head->next = NULL;
-
-    
-    //初始化每个CPU的page，都为空NULL
-    for(size_t i=0;i<cpu_count();i++){
-        cpu_page[i]=NULL;
-        init_lock(&cpu_page_lock[i]);
+    for(int i=0;i<cpu_cnt;i++){
+        cpu_ptr[i]=heap.start+i*cpu_sz;
+        cpu_ptr_end[i]=heap.start+(i+1)*cpu_sz;
     }
 
+    printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+
     init_lock(&heap_lock);
-    printf("PMM: init done\n");
+    for(int i=0;i<cpu_cnt;i++){
+        init_lock(&cpu_lock[i]);
+        printf("CPU #%d : [%p, %p)\n", i, cpu_ptr[i], cpu_ptr_end[i]);
+    }
+    
+    //printf("PMM: init done\n");
     //锁不分配给CPU，分配给slab_page.
 
 
