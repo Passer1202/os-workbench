fa4ebb1d37b92c53c4e7e4c8da64b773a96da02a
tracer-nju <tracer@nju.edu.cn>
2024-07-10 21:26:40 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:40 up 29 min,  1 user,  load average: 0.55, 0.48, 0.53

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -258,7 +258,7 @@ void test_pmm() {
     alloc(10);
     alloc(32);
     //alloc(16777216);
-    //alloc(4096);
+    alloc(4096);
     //alloc(5000);
     //atomic{
     //printf("PMM: test passed\n");
