8715c8237a688f1f74afc726a5e32926a85fa4f9
tracer-nju <tracer@nju.edu.cn>
2024-07-10 20:47:16 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:16 up  4:07,  1 user,  load average: 0.74, 1.04, 1.01

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -110,12 +110,12 @@ static void *kalloc(size_t size) {
             page->magic=MAGIC_NUM;
             page->cnt=0;
             page->val=(DATA_SIZE/sz);
-
+            page->val=(DATA_SIZE/sz);
             acquire_lock(&heap_lock);
                 printf("sz:%d\n",sz);
                 printf("DATA_SIZE:%d\n",DATA_SIZE);
                 printf("page->val:%d %d\n",(DATA_SIZE/sz),page->val);
-                page->val=(DATA_SIZE/sz);
+                //page->val=(DATA_SIZE/sz);
                 assert(page->val>0);
             release_lock(&heap_lock);
 
