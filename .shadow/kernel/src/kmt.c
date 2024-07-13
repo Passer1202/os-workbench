223a92162dc41324efbc5a78963bce3c84884785
tracer-nju <tracer@nju.edu.cn>
2024-07-13 14:53:18 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:18 up 49 min,  1 user,  load average: 0.59, 0.72, 0.71

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -8,16 +8,20 @@
 
 
 static Context *kmt_context_save(Event ev, Context *ctx){
+    
     return NULL;
 }
 
 static Context *kmt_schedule(Event ev,Context *ctx){
+    
     return NULL;
 }
 
 static void kmt_init(){
+
     os->on_irq(INT_MIN, EVENT_NULL, kmt_context_save);
     os->on_irq(INT_MAX, EVENT_NULL, kmt_schedule);
+
 }
 
 static int  kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *arg){
