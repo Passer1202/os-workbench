69aabdd4e16666d3dea2dbfcc2e6992c37004226
tracer-nju <tracer@nju.edu.cn>
2024-08-07 21:22:46 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:46 up  2:39,  1 user,  load average: 1.61, 1.13, 0.94

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -83,8 +83,8 @@ static int xxxx=0;
 static Context *kmt_context_save(Event ev, Context *ctx){
     
     spin_lock(&task_lock);
-    printf("xxxx: %d\n",xxxx);
-    xxxx++;
+    //printf("xxxx: %d\n",xxxx);
+    //xxxx++;
     assert(xxxx%2==1);
 
     assert(ienabled()==0);//中断关闭
@@ -110,8 +110,8 @@ static Context *kmt_schedule(Event ev,Context *ctx){
     //static int x=0;
     //printf("%d\n",x);
     //x++;
-    xxxx++;
-    printf("xxxx: %d\n",xxxx);
+    //xxxx++;
+    //printf("xxxx: %d\n",xxxx);
     assert(xxxx%2==0);
     assert(ienabled()==0);//中断关闭
 
@@ -173,8 +173,8 @@ static void current_init(){
 static void kmt_init(){
 
     //注册中断处理函数
-    os->on_irq(INT_MIN, EVENT_NULL, kmt_context_save);
-    os->on_irq(INT_MAX, EVENT_NULL, kmt_schedule);
+    os->on_irq(INT_MAX, EVENT_NULL, kmt_context_save);
+    os->on_irq(INT_MIN, EVENT_NULL, kmt_schedule);
 
     //初始化每个cpu上的current
     current_init();
