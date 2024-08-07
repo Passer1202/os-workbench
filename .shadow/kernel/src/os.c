33003332c747b32db8c3ff29b49200fec686bff4
tracer-nju <tracer@nju.edu.cn>
2024-08-07 18:57:49 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:57:49 up 14 min,  1 user,  load average: 0.68, 0.84, 0.55

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -76,7 +76,7 @@ sem_t empty, fill;
 void producer(void *arg) {
     while (1) {
         P(&empty);
-        //putch('(');
+        putch('(');
         V(&fill);
     }
 }
@@ -118,7 +118,7 @@ static void os_init() {
 
 #ifdef TEST_3
     kmt->sem_init(&empty, "empty", 1);  // 缓冲区大小为 5
-    kmt->sem_init(&fill, "fill", 0);
+    kmt->sem_init(&fill, "fill", 1);
 
     //for (int i = 0; i < 4; i++)  // 4 个生产者
         kmt->create(task_alloc(), "producer", producer, NULL);
