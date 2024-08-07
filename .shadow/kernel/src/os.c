dbf33deff8c07c62c0a6cdcf7393347ee26ff8fb
tracer-nju <tracer@nju.edu.cn>
2024-08-07 18:57:30 +0800

    Linux lhj-XiaoXinAir 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:57:30 up 14 min,  1 user,  load average: 0.87, 0.88, 0.56

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -76,14 +76,14 @@ sem_t empty, fill;
 void producer(void *arg) {
     while (1) {
         P(&empty);
-        putch('(');
+        //putch('(');
         V(&fill);
     }
 }
 void consumer(void *arg) {
     while (1) {
         P(&fill);
-         putch(')');
+        putch(')');
         V(&empty);
     }
 }
